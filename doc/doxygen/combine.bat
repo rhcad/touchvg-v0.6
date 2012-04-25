90bac3a87c6ab37c74eca436d6224ea0f88baf10
Zhang Yungui <rhcad@hotmail.com>
2012-04-25 13:34:30 +0800

    Add combine.bat

--- doc/doxygen/combine.bat
+++ doc/doxygen/combine.bat
@@ -1,4 +1,6 @@
-set destpath=..\..\build\touchdraw
+set destpath=..\..\build\all
+
+del /Q/S  %destpath%
 
 xcopy ..\..\core\include\geom\*.* %destpath%\geom\ /Y
 xcopy ..\..\core\include\graph\*.* %destpath%\graph\ /Y
@@ -11,11 +13,16 @@ xcopy ..\..\win32\include\graphwin\*.* %destpath%\graphwin\ /Y
 xcopy ..\..\win32\src\graphwin\*.* %destpath%\graphwin\ /Y
 xcopy ..\..\win32\src\SimpView\*.* %destpath%\MfcApp\ /Y
 
+mkdir %destpath%\GraphView.win
+move /Y %destpath%\MfcApp\BaseView.* %destpath%\GraphView.win\
+move /Y %destpath%\MfcApp\Step*View.* %destpath%\GraphView.win\
+del  /Q %destpath%\MfcApp\StdAfx.* %destpath%\MfcApp\resource.h
+
 xcopy ..\..\iPad\Graph2d\Headers\*ios.h %destpath%\graphios\ /Y
 xcopy ..\..\iPad\Graph2d\Sources\*.cpp %destpath%\graphios\ /Y
 
-xcopy ..\..\iPad\GraphView\Headers\*.h %destpath%\viewios\ /Y
-xcopy ..\..\iPad\GraphView\Sources\*.* %destpath%\viewios\ /Y
+xcopy ..\..\iPad\GraphView\Headers\*.h %destpath%\GraphView.ios\ /Y
+xcopy ..\..\iPad\GraphView\Sources\*.* %destpath%\GraphView.ios\ /Y
 
-xcopy ..\..\iPad\iPadDraw\Headers\*.h %destpath%\viewios\ /Y
+xcopy ..\..\iPad\iPadDraw\Headers\*.h %destpath%\iPadApp\ /Y
 xcopy ..\..\iPad\iPadDraw\Sources\*.* %destpath%\iPadApp\ /Y
