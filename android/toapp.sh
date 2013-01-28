a7074acbbe37828849dbcbd5af2274b3b1b0f84c
Zhang Yungui <rhcad@hotmail.com>
2013-01-28 11:21:25 +0800

    Add android/vglibs.

--- android/toapp.sh
+++ android/toapp.sh
@@ -1,32 +1,32 @@
 # Type 'sh toapp.sh' to make skiaview.jar for Android.
-# Need to copy $ANDROID_SDK_HOME/platforms/android-?/android.jar to libs/ .
+# Need to copy $ANDROID_SDK_HOME/platforms/android-?/android.jar to vglibs/ .
 #
-if [ -f libs/android.jar ]
+if [ -f vglibs/android.jar ]
 then
     python utf8togbk.py # the javac application don't support UTF8 files.
     cd skiaview/src/touchvg/jni; javac *.java;
     cd ../..; jar -cf vgjni.jar touchvg/jni/*.class;
-    cd touchvg/view; javac -cp ../../vgjni.jar:../../../../libs/android.jar *.java;
+    cd touchvg/view; javac -cp ../../vgjni.jar:../../../../vglibs/android.jar *.java;
     cd ../..; jar -cfv skiaview.jar touchvg/view/*.class touchvg/jni/*.class;
-    cd ..; mv -v src/skiaview.jar ../libs;
+    cd ..; mv -v src/skiaview.jar ../vglibs;
     rm -rf src/touchvg/jni/*.class;
     rm -rf src/touchvg/view/*.class;
     rm -rf src/*.jar;
-    cp -v libs/armeabi/libskiaview.so ../libs/armeabi
+    cp -v libs/armeabi/libskiaview.so ../vglibs/armeabi
     cd ..
 
     if [ -n "$TOUCHVG_ANDROID_APP" ]
     	then
-    	cp -v libs/skiaview.jar $TOUCHVG_ANDROID_APP/libs;
-    	cp -v libs/armeabi/libskiaview.so $TOUCHVG_ANDROID_APP/libs/armeabi;
+    	cp -v vglibs/skiaview.jar $TOUCHVG_ANDROID_APP/libs;
+    	cp -v vglibs/armeabi/libskiaview.so $TOUCHVG_ANDROID_APP/libs/armeabi;
     	else
     	echo "you have not the environment variable TOUCHVG_ANDROID_APP"
     fi
     python restore_utf8.py
 else
-    echo "Warning: Need to copy android.jar from ANDROID_SDK_HOME/platforms to android/libs/ ."
+    echo "Warning: Need to copy android.jar from ANDROID_SDK_HOME/platforms to android/vglibs/ ."
     if [ -n "$TOUCHVG_ANDROID_APP" ]
         then
-    	cp -v libs/armeabi/libskiaview.so $TOUCHVG_ANDROID_APP/libs/armeabi
+    	cp -v vglibs/armeabi/libskiaview.so $TOUCHVG_ANDROID_APP/libs/armeabi
     fi
 fi
