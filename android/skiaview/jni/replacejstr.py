3858ec4087de0a8044bfc7e0b99b0ff5f27bddd7
unknown <zhangyg@ZHANGYG.HOLD.FOUNDER.COM>
2013-01-28 13:28:38 +0800

    Update with SWIG 2.0.9 .

--- android/skiaview/jni/replacejstr.py
+++ android/skiaview/jni/replacejstr.py
@@ -3,6 +3,6 @@ import os
 file = os.path.abspath('skiaview_java_wrap.cpp')
 text = open(file).read()
 text = text.replace('jstring jname = 0 ;', 'jstring jname = 0; TmpJOBJ jtmp(jenv, &jname);')
-text = text.replace('swig_disconnect_director_self("swigDirectorDisconnect");', \
-                    '//swig_disconnect_director_self("swigDirectorDisconnect");')
+#text = text.replace('swig_disconnect_director_self("swigDirectorDisconnect");', \
+#                    '//swig_disconnect_director_self("swigDirectorDisconnect");')
 open(file, 'w').write(text)
